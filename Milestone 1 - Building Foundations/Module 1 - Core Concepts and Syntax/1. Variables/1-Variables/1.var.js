5b35c4f0849cecfd99ae950d214f30aeb0a8a967
Hasnat Shahriyar <121642545+hasnat-shahriyar@users.noreply.github.com>
2024-02-07 16:05:25 +0600

    Add files via upload

---
+++ Milestone 1 - Building Foundations/Module 1 - Core Concepts and Syntax/1. Variables/1-Variables/1.var.js
@@ -0,0 +1,4 @@
+var x = 10;
+console.log(x);
+
+// var: Before ES6 (ECMAScript 2015), var was the primary way to declare variables in JavaScript. Variables declared with var have function-level scope, which means they are accessible within the function in which they are defined, or globally if defined outside of any function.
---
+++ Milestone 1 - Building Foundations/Module 1 - Core Concepts and Syntax/1. Variables/1-Variables/4.var-let.js
@@ -0,0 +1,5 @@
+let a = 5;
+a = 10; // Reassigning the value of 'a'
+console.log(a); // Output: 10
+
+// Variables declared with var and let can be reassigned:
