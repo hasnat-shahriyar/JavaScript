4aa2dcf96a42be1c1225dae41299559352761601
Hasnat Shahriyar <121642545+hasnat-shahriyar@users.noreply.github.com>
2024-02-07 16:08:45 +0600

    Delete Milestone 1 - Building Foundations/Module 1 - Core Concepts and Syntax/1. Variables directory

--- Milestone 1 - Building Foundations/Module 1 - Core Concepts and Syntax/1. Variables/2.let.js
+++
@@ -1,4 +0,0 @@
-let y = 20;
-console.log(y); // Output: 20
-
-// Introduced in ES6, let allows you to declare block-scoped variables. Block-scoped variables are only accessible within the block in which they are defined (e.g., within loops, if statements, or functions).
--- Milestone 1 - Building Foundations/Module 1 - Core Concepts and Syntax/1. Variables/4.var-let.js
+++
@@ -1,5 +0,0 @@
-let a = 5;
-a = 10; // Reassigning the value of 'a'
-console.log(a); // Output: 10
-
-// Variables declared with var and let can be reassigned:
