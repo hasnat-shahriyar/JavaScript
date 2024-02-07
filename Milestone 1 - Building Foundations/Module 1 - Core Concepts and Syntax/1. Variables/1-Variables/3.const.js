72c0b971d4d34f4f781df6d5e91736d83af61c3f
Hasnat Shahriyar <121642545+hasnat-shahriyar@users.noreply.github.com>
2024-02-07 16:06:26 +0600

    Delete Milestone 1 - Building Foundations/Module 1 - Core Concepts and Syntax/1. Variables/1-Variables directory

--- Milestone 1 - Building Foundations/Module 1 - Core Concepts and Syntax/1. Variables/1-Variables/3.const.js
+++
@@ -1,4 +0,0 @@
-const z = 30;
-console.log(z); // Output: 30
-
-// Also introduced in ES6, const is used to declare constants, which are variables that cannot be reassigned a new value after their initial assignment. Like let, const is block-scoped.
