5cff9ab849a963b9ecc0124edbd015c69cd1543b
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2024-02-29 16:59:50 +0600

    let cannot be redeclared

--- Milestone 1 - Building Foundations/Module 1 - Core Concepts and Syntax/1-Variables/2.let.js
+++ Milestone 1 - Building Foundations/Module 1 - Core Concepts and Syntax/1-Variables/2.let.js
@@ -1,4 +1,9 @@
 let y = 20;
 console.log(y); // Output: 20
 
+/*
+let x = "declared";
+let x = 'redeclare'; //let cannot be redeclared.
+*/
+
 // Introduced in ES6, let allows you to declare block-scoped variables. Block-scoped variables are only accessible within the block in which they are defined (e.g., within loops, if statements, or functions).
