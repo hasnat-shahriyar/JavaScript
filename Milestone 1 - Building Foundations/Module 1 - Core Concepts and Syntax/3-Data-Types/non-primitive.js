02f0b94b57251b8f8d5ed8a7aab3847a5847d660
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2024-03-04 03:26:19 +0600

    non-primitive/reference data types

--- Milestone 1 - Building Foundations/Module 1 - Core Concepts and Syntax/3-Data-Types/non-primitive.js
+++ Milestone 1 - Building Foundations/Module 1 - Core Concepts and Syntax/3-Data-Types/non-primitive.js
@@ -1,1 +1,8 @@
 // Non-Primitive/Reference Data Types:
+function modifyObject(obj) {
+    obj.property = "modified";
+}
+
+let myObject = { property: "original" };
+modifyObject(myObject);
+console.log(myObject.property); // Output: "modified"
